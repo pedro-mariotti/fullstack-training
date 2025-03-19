8f3054aa40eab92d0d257b638d5b08d93d0e4447
Pedro Mariotti <mariottipedro14@gmail.com>
2025-03-18 22:11:42 -0300

    Botão que limpa imagem perfil

--- conference-ticket-generator-main/public/index.js
+++ conference-ticket-generator-main/public/index.js
@@ -8,6 +8,7 @@ const filledUserImageInputDiv = document.getElementById("filled-pfp");
 const unfilledUserImageInputDiv = document.getElementById("unfilled-pfp");
 const removePfpButton = document.getElementById("remove-pfp");
 const pfpTempImage = document.getElementById("pfp-temp-image");
+var filesPfp = null;
 
 var userProfilePic = null;
 var userName = null;
@@ -23,20 +24,28 @@ userForm.addEventListener("submit", (e) => {
   userGithub = formData.get("github");
 });
 userImageInput.onchange = (e) => {
-  const filesPfp = e.target.files;
+  filesPfp = e.target.files;
   const reader = new FileReader();
   console.log(filesPfp);
+  console.log(userImageInput.value);
+
   if (filesPfp.length > 0) {
     reader.onload = function (event) {
-      userProfilePic.src = event.target.result;
+      pfpTempImage.src = event.target.result;
     };
     reader.readAsDataURL(filesPfp[0]);
 
-    filledUserImageInputDiv.style.display = "flex ";
+    filledUserImageInputDiv.style.display = "flex";
     unfilledUserImageInputDiv.style.display = "none";
   } else {
     filledUserImageInputDiv.style.display = "none";
     unfilledUserImageInputDiv.style.display = "block";
   }
 };
-removePfpButton.onclick = () => {};
+removePfpButton.onclick = () => {
+  pfpTempImage.src = "";
+  console.log(userImageInput.value);
+  userImageInput.value = "";
+  unfilledUserImageInputDiv.style.display = "block";
+  filledUserImageInputDiv.style.display = "none";
+};
